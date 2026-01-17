192723beae12d9c32a5015ecb8677e4a73796bb5
Maurice Laveaux <m.laveaux@tue.nl>
2026-01-17 13:03:22 +0100

    Add the 'bundled' feature to build a statically linked mt-kahypar.

--- build.rs
+++ build.rs
@@ -1,9 +1,51 @@
 use std::env;
 use std::path::PathBuf;
 
+use bindgen::{Builder, EnumVariation, RustEdition, RustTarget};
+
 fn main() {
     let link_type;
+    
+    if cfg!(feature = "bundled") {
+        link_type = "static";
 
+        // Use the `cmake` crate to build mt-kahypar C++ library.
+        let mut dst = cmake::Config::new("mt-kahypar")
+            .define("BUILD_SHARED_LIBS", "OFF") // Build a static library.
+            .define("KAHYPAR_STATIC_LINK_DEPENDENCIES ", "ON")
+            .define("KAHYPAR_DOWNLOAD_TBB", "ON") // Use built-in TBB.
+            .define("KAHYPAR_DOWNLOAD_BOOST", "ON") // Use built-in Boost.
+            .define("KAHYPAR_STATIC_LINK_TBB ", "ON") // Statically link TBB.
+            .define("KAHYPAR_DISABLE_HWLOC", "ON") // Disable hwloc to avoid dependency.
+            .build();
+        dst.push("build/lib");
+
+        cargo_emit::rustc_link_search!(dst.display() => "native");
+        
+        // Generate bindings for the C++ interface using bindgen.
+        let bindings = Builder::default()
+            // The input header we would like to generate
+            // bindings for.
+            .header("mt-kahypar/include/mtkahypar.h")
+            // Tell cargo to invalidate the built crate whenever any of the
+            // included header files changed.
+            .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
+            // Sets various defaults for bindgen.
+            .default_enum_style(EnumVariation::Rust { non_exhaustive: false })
+            .rust_target(RustTarget::stable(73, 0).expect("Unsupported Rust version"))
+            .rust_edition(RustEdition::Edition2021)
+            // Finish the builder and generate the bindings.
+            .generate()
+            // Unwrap the Result and panic on failure.
+            .expect("Unable to generate bindings");
+        
+        // Write the bindings to the $OUT_DIR/bindings.rs file.
+        let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+        bindings
+            .write_to_file(out_path.join("bindings.rs"))
+            .expect("Couldn't write bindings!");
+    }
+    else
     {
         link_type = "dylib";
 
@@ -28,8 +70,8 @@ fn main() {
         }
 
         println!("rerun-if-env-changed=MTKAHYPAR_DIR");
-    }
+    }   
 
     // Finally, link the library.
-    println!("cargo:rustc-link-lib={}=mtkahypar", link_type);
+    cargo_emit::rustc_link_lib!("mtkahypar" => link_type);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,9 @@
+//! Suppress various warnings from the generated bindings.
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![allow(unused)]
 
+// When the "bundled" feature is enabled, include the generated bindings.
+#[cfg(feature = "bundled")]
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
